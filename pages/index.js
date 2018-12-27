7dae718d47a297c3937663312010d436cc33d713
Zak Auerbach <zak@codeforamerica.org>
2018-12-26 17:02:48 -0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hi, this is zaksoup.cloud</p>
+      </div>
+    )
+  }
+}
+
+export default Index
