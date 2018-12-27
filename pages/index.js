bb1ca7027c88828eac9d9e06db59d096e84a02d8
Zak Auerbach <zak@codeforamerica.org>
2018-12-26 17:06:29 -0800

    Update hello text

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ class Index extends Component {
   render() {
     return (
       <div>
-        <p>Hi, this is zaksoup.cloud</p>
+        <p>Hi, this is zaksoup.{'{'}cloud, space{'}'}. What's up, my dudes!?</p>
       </div>
     )
   }
