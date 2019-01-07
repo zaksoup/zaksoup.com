b045772279e56837f6d0f6eece9e3b693873f18a
Zak Auerbach <zak@codeforamerica.org>
2019-01-07 12:21:54 -0800

    Add zaksoup.com

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ class Index extends Component {
   render() {
     return (
       <div>
-        <p>Hi, this is zaksoup.{'{'}cloud, space{'}'}. What's up, my dudes!?</p>
+        <p>Hi, this is zaksoup.{'{'}com, cloud, space{'}'}. What's up, my dudes!?</p>
         <p>One day, I'll have something interesting here</p>
         <p>but for now, this is all you'll get</p>
       </div>
